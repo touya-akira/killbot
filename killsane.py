8cfd2c702164bc80505f7e018d1ba59baf697b4d
touya-akira <touya.akira@yandex.com>
2013-02-02 18:08:39 +0400

    v1.6

---
+++ killsane.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+#KillingJenny 1.6
+
+from __future__ import division
+import random
+import os
+import time
+armed = False
+globalcount = 150
+killchance = 50.0
+killmod = 23.5
+KILLCHAN = "#kill"
+SERVICENICKS = ["Boxxy"]
+BOTNICK = ""
+OPERNICK = ""
+OPERPASS = ''
+EXCEMPTLIST = ['katie', BOTNICK, SERVICENICK]
+
+def arm(phenny, input):
+	if not input.admin: return
+	phenny.write(['oper',OPERNICK,OPERPASS])
+	phenny.write(['mode','BOTNICK','+H'])
+	global armed
+	armed = True
+	phenny.say('Pheer. '+BOTNICK+' haz o:line!')
+arm.commands = ['arm']
+arm.priority = 'high'
+
+def disarm(phenny, input):
+	if not input.admin: return
+	phenny.write(['mode','BOTNICK','-o'])
+	phenny.say('I am disarmed :/')
+	global armed
+	armed = False
+disarm.commands = ['disarm']
+disarm.priority = 'high'
+
+def setmod(phenny, input):
+	if not input.admin: return
+	global killmod
+	killmodo = killmod
+	killmod = float(input.group(2))
+	phenny.say('kill modifier changed from '+str(killmodo)+' to '+str(killmod))
+setmod.commands = ['setmod']
+
+def astatus(phenny, input):
+	global armed
+	if armed == True: phenny.say('I am armed.')
+	else: phenny.say('I am disarmed.')
+astatus.commands = ['status']
+
+def dokill(phenny, input):
+	global killquit
+	global killmod
+	global globalcount
+	global killchance
+	for name in EXCEMPTLIST:
+		if name == input.nick: return
+	if input.admin: 
+		phenny.say('d\'aww, I would never /kill you, '+input.nick+'!')
+		return
+	random.seed()
+	killchance = killmod/globalcount
+	phenny.say('Ohai, '+input.nick+'! Let\'s roll the dice of destiny. With a current killing chance of '+str(100*(1-killchance))+'%, you will need a result less than '+str(killchance)+'. Rolling...')
+	time.sleep(1)
+	randn = random.uniform(0.0,1.0)
+	if randn > killchance:
+		killquit = True
+		phenny.say('Nope. Your result: '+str(randn)+'. Die well :-)')
+		phenny.write(['kill',input.nick,'You lost the #kill game (Killing chance: '+str(100*(1-killchance))+'%)'])
+	else:
+		phenny.say(str(randn)+' rolled!')
+		phenny.say('Yay, you won the #kill game. Welcome to the channel!')
+		phenny.write(['mode','#kill','+v',input.nick])
+		with open ('killscore.txt','r') as f:
+			oldhs = f.readline()
+			oldname = f.readline()
+			olddate = f.readline()
+		ohs = float(oldhs)
+		if 100*(1-killchance) > ohs:
+			phenny.say('\002NEW HIGHSCORE!\002 Old Highscore: '+oldhs+' by '+oldname+' ('+olddate+')')
+			msg = 'Survived joining #kill with a killing chance of '+str(100*(1-killchance))+'% (All-Time Highscore!)'
+			phenny.write(['SWHOIS', input.nick, msg])
+			hstime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+			hsval = str(100*(1-killchance))
+			with open ('killscore.txt','w') as f:
+				f.write(hsval+'\n')
+				f.write(input.nick+'\n')
+				f.write(hstime+'\n')
+			msg = input.nick+' set a new highscore for #kill!: '+str(100*(1-killchance))+'%. Check if you can reach a new higscore by typing .killstatus in a channel with katie.'
+			phenny.write(['WALLOPS',msg])
+		else:
+			msg = 'Survived joining #kill with a killing chance of '+str(100*(1-killchance))+'%'
+			phenny.write(['SWHOIS', input.nick, msg])
+		phenny.say('Awarded SWHOIS title. Congratulations, '+input.nick)
+dokill.event = 'JOIN'
+dokill.rule = r'.*'
+dokill.priority = 'high'
+
+def updatecount(phenny, input):
+	phenny.write(['LUSERS'])
+updatecount.commands = ['update']
+
+def getcount(phenny, input):
+	global killchance
+	global globalcount
+	global killmod
+	line = input.group().split()
+	globalcount = int(line[3])
+	killchance = killmod/globalcount
+	msg = 'Current chance of being killed: '+str(100*(1-killchance))+'%'
+	phenny.write(['privmsg','#kill',msg])
+	with open ('killscore.txt','r') as f:
+		hsval  = f.readline()
+		hsname = f.readline()
+		hstime = f.readline()
+	msg = 'All-Time Highscore: '+hsval+'%, set by '+hsname+' ('+hstime+')'
+	phenny.write(['privmsg','#kill',msg])
+getcount.event = "266"
+getcount.rule = r'.*'
+
+def katie_inq(phenny, input):
+	if input.nick != 'katie': return
+	phenny.write(['LUSERS'])
+	time.sleep(1)
+	killchance = killmod/globalcount
+	phenny.reply(str(100*(1-killchance)))
+katie_inq.event = 'PRIVMSG'
+katie_inq.rule = r'.*'
